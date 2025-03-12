ff6a1cbb50820cdf5b44f4cdc19085940d8720fa
d5c5ceb0 <d5c5ceb0@gmail.com>
2025-03-12 11:14:30 +0800

    remove unused package

--- operator/db_sql/src/error.rs
+++
@@ -1,8 +0,0 @@
-use thiserror::Error;
-
-#[derive(Error, Debug)]
-pub enum DatabaseError {
-    //todo add error type
-}
-
-pub type DatabaseResult<T> = Result<T, DatabaseError>;
--- operator/runer/src/lib.rs
+++
@@ -1,6 +0,0 @@
-pub mod operator;
-pub mod storage;
-pub mod node_factory;
-pub mod handler;
-pub mod api;
-pub mod cli;
